4b2caa4c8dabd8aa2734fe225159dd9ca4eeb21c
kainat42000 <bscs2312372@szabist.pk>
2025-05-06 12:45:56 +0000

    message

--- STUDENTAPI/index.js
+++ STUDENTAPI/index.js
@@ -15,14 +15,62 @@ app.get('/',async(requestAnimationFrame,res)=>{
         );
     }
 })
-app.get('/gettotalstd',async(requestAnimationFrame,res)=>{
+app.get('/student',async(requestAnimationFrame,res)=>{
     try{
         const result = await Pool.query('select * from student');
         res.json(result.rows);
     }catch(err){
         res.status(500).json({Error:err.message});
     }
+});
+app.get('/jobs',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(job_id) from jobs');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/countries',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(country_id) from countries');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/regions',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(region_id) from regions');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
 })
+app.get('/employees',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(employee_id) from employees');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/job_history',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(job_id) from job_history');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/departments',async(requestAnimationFrame,res)=>{
+    try{
+        const result = await Pool.query('select count(department_id) from departments');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT,() =>{
